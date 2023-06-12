21d6dc00ef87008885b532dda0dcc1acb03d8660
SeeSharpSi <silastompkins@outlook.com>
2023-06-12 10:14:30 -0400

    solved day 1

--- day-1/calorie_counter.go
+++ day-1/calorie_counter.go
@@ -14,45 +14,85 @@ func check(e error) {
 	}
 }
 
-func counter(fileName string) (calories_per_elf []int, elfs_cals [][]int) {
+func counter(fileName string) (elfs_cals [][]int) {
 	file, err := os.Open(fileName)
 	check(err)
 	scanner := bufio.NewScanner(file)
-	calories_per_elf = make([]int, 1)
 	elfs_cals = append(elfs_cals, []int{})
-
 	for scanner.Scan() {
 		if scanner.Text() != "" {
 			scan_conv, err := strconv.Atoi(scanner.Text())
 			check(err)
-			calories_per_elf[len(calories_per_elf) - 1] += scan_conv
-			elfs_cals[len(elfs_cals) - 1] = append([]int(elfs_cals[len(elfs_cals) - 1]), scan_conv)
+			elfs_cals[len(elfs_cals)-1] = append([]int(elfs_cals[len(elfs_cals)-1]), scan_conv)
 		} else {
-			calories_per_elf = append(calories_per_elf, 0)
 			elfs_cals = append(elfs_cals, []int{})
 		}
 	}
 	return
 }
 
-func findNumber(array []int, needle int) (x int, found bool) {
-    for i, v := range array {
-        fmt.Println(v)
-        if v == needle {
-            return i, true
+func add_grouped_cals(array [][]int) (grouped []int) {
+    for _, v := range array {
+        temp := 0
+        for _, va := range v {
+            temp += va
         }
+        grouped = append(grouped, temp)
     }
-    return -1, false
+    return
+}
+
+func findNumber(array []int, needle int) (x int, newArray []int) {
+	for i, v := range array {
+		if v == needle {
+			temp := make([]int, 0)
+			temp = append(temp, array[:i]...)
+			temp = append(temp, array[i+1:]...)
+			return i, temp
+		}
+	}
+	return -1, []int{}
+}
+
+func eat(array [][]int, index int) [][]int {
+	first_dimension := array[index]
+	sort.Slice(first_dimension, func(i, j int) bool {
+		return first_dimension[i] > first_dimension[j]
+	})
+	_, temp := findNumber(array[index], first_dimension[0])
+	array[index] = temp
+	return array
 }
 
 func main() {
-	sorted_cals, elf_cals := counter("./calories") // i don't need the ./ here but might as well
-    unsorted_cals := make([]int, len(sorted_cals))
-    copy(unsorted_cals, sorted_cals)
-	sort.Slice(sorted_cals, func(i, j int) bool {
-		return sorted_cals[i] > sorted_cals[j]
-	})
-    index, _ := findNumber(unsorted_cals, sorted_cals[0])
-	fmt.Println(unsorted_cals, elf_cals)
-    fmt.Println(index)
+	elf_cals := counter("./calories") // i don't need the ./ here but might as well
+	unsorted_cals := make([]int, 0)
+	for i := range elf_cals {
+		for _, va := range elf_cals[i] {
+			unsorted_cals = append(unsorted_cals, va)
+		}
+	}
+	for len(unsorted_cals) > 0 {
+		sorted_cals := make([]int, len(unsorted_cals))
+		copy(sorted_cals, unsorted_cals)
+		sort.Slice(sorted_cals, func(i, j int) bool {
+			return sorted_cals[i] > sorted_cals[j]
+		})
+        added_cals := add_grouped_cals(elf_cals)
+        sorted_added_cals := make([]int, len(added_cals))
+        copy(sorted_added_cals, added_cals)
+        // fmt.Println(sorted_added_cals)
+        sort.Slice(sorted_added_cals, func(i, j int) bool {
+            return sorted_added_cals[i] > sorted_added_cals[j]
+        })
+        fmt.Println(sorted_added_cals)
+		index, _ := findNumber(added_cals, sorted_added_cals[0])
+		unsorted_cals = nil
+		elf_cals = eat(elf_cals, index)
+		for _, v := range elf_cals {
+			for _, va := range v {
+				unsorted_cals = append(unsorted_cals, va)
+			}
+		}
+	}
 }
